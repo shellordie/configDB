6d6dcef43bccb4acaddcf0363322a76449681bb7
shellordie <shellordie@gmail.com>
2023-09-17 08:49:18 -1200

    create and save

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
        # long_description_content_type="text/markdown",
         #long_description=open("README.md","r",encoding="utf-8").read(),
         packages=find_packages(),
-        install_requires=["ini-parser"],  
+        #install_requires=["ini-parser"],  
         #keywords=["Computer-vision",
         #"preprocessing","Images","Dataset","visionner"],
         #classifiers= [
--- virvadb/main.py
+++ virvadb/main.py
@@ -1,15 +1,24 @@
-import ini
+from configparser import ConfigParser
 import os
 
 class Virvadb():
     def __init__(self,name):
         self.current_path=os.getcwd()
         self.db_folder_path=r"{}/db".format(self.current_path)
-        self.db_path=r"{}/{}".format(self.db_folder_path,name)
+        self.db_path=r"{}/{}.ini".format(self.db_folder_path,name)
         if not os.path.exists(self.db_folder_path):
             os.mkdir(self.db_folder_path)
         if not os.path.exists(self.db_path):
-            with open(self.db_path+".ini","a+") as fp:
+            with open(self.db_path,"a+") as fp:
                 pass
+
+    def create(self,dico):
+        self.config=ConfigParser()
+        sid=0
+        self.config[sid]=dico
+
+    def save(self):
+        with open(self.db_path,"a+") as f:
+            self.config.write(f)
 
 
